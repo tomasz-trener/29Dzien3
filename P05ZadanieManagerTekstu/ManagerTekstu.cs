16eb3071204e4054f9a2d2295222e360e530a412
kurs <kurs@DESKTOP-284UO99>
2024-11-04 12:16:48 +0100

    q

---
+++ P05ZadanieManagerTekstu/ManagerTekstu.cs
@@ -0,0 +1,50 @@
+﻿using P05ZadanieManagerTekstu.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P05ZadanieManagerTekstu
+{
+    internal class ManagerTekstu
+    {
+        private string[] znajdzWyrazyOPodanejDlugosci(int dlugosc, string zdanie)
+        {
+            HashSet<string> znalezione = new HashSet<string>(); // lista bez powtórzeń  
+            foreach (var w in zdanie.Split(' '))
+            {
+                if(w.Length == dlugosc)
+                    znalezione.Add(w);
+            }
+            return znalezione.ToArray();
+        }
+
+        public string ZnajdzNajdluzszyWyraz(string zdanie)
+        {
+            if (zdanie.Length < 1)
+                throw new ZbytKrotkieZdanieException();
+
+            if (string.IsNullOrWhiteSpace(zdanie))
+                throw new Exception("Nie mozesz podac samych spacji");
+
+            string[] wyrazy = zdanie.Split(' ');
+            string aktualnieNajdluzszy = string.Empty;
+            foreach (var w in wyrazy)
+                if(w.Length > aktualnieNajdluzszy.Length)
+                    aktualnieNajdluzszy = w;
+
+            return aktualnieNajdluzszy;
+        }
+
+        public string[] ZnajdzWszystkieNajdluzszeWyrazy(string zdanie)
+        {
+            if (string.IsNullOrEmpty(zdanie))
+                throw new ArgumentException("Zdanie nie moze byc puste ani  zawierac tylko białe znaki");
+    
+            string najdluzszy = ZnajdzNajdluzszyWyraz(zdanie);
+            return znajdzWyrazyOPodanejDlugosci(najdluzszy.Length, zdanie);
+        }
+
+    }
+}
--- P05ZadanieManagerTekstu/Program.cs
+++ P05ZadanieManagerTekstu/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using P05ZadanieManagerTekstu.Exceptions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,36 @@ namespace P05ZadanieManagerTekstu
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Podaj zdanie");
+            string zdanie = Console.ReadLine();
+            ManagerTekstu mt = new ManagerTekstu();
+
+            try
+            {
+                string pierwszyNajdluzszy =
+                    mt.ZnajdzNajdluzszyWyraz(zdanie);
+                Console.WriteLine($"WPierwszy najdluzszy: {pierwszyNajdluzszy}");
+
+                string[] wyrazy = mt.ZnajdzWszystkieNajdluzszeWyrazy(zdanie);
+
+                Console.WriteLine($"Wszystkie najdluzsze wyrazy: {string.Join(",",wyrazy)}");
+            }
+            catch (ZbytKrotkieZdanieException)
+            {
+                Console.WriteLine("Zbyt krótkie zdanie");
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Zdanie nie poprawne");
+            }
+            catch (Exception ex)
+            {
+                //  Console.WriteLine(ex.Message);
+                Console.WriteLine("Podałeś złe zdanie");
+            }
+
+            Console.ReadKey();
+
         }
     }
 }
