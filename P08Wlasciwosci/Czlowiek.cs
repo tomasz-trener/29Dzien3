f40b6167cbf4726a2d9369edd36aba5a9ffafa97
kurs <kurs@DESKTOP-284UO99>
2024-11-04 14:33:35 +0100

    q

---
+++ P08Wlasciwosci/Czlowiek.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P08Wlasciwosci
+{
+    internal class Czlowiek
+    {
+        public string Nazwisko { get; set; } // Wlasiwosc 
+
+
+        private string imie; // pole 
+
+        public string Imie
+        {
+            get 
+            { 
+                string s= imie.ToLower();
+                return s; 
+            }
+            set { imie = value; }
+        }
+
+        public string ImieNazwisko
+        {
+            get
+            {
+                string s1 = imie.ToUpper();
+                string s2 = Nazwisko.ToUpper();
+                return s1 + " " + s2;
+            }
+            set
+            {
+                string[] s= value.Split(' ');
+                imie = s[0];
+                Nazwisko = s[1];
+            }
+        }
+
+
+        public string PrzedstawSie()
+        {
+            return "Nazywam sie" + imie + " " + Nazwisko;
+        }
+    }
+}
---
+++ P08Wlasciwosci/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P08Wlasciwosci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kalkulator kalkulator = new Kalkulator();
+            kalkulator.Kolor = "czerwony";
+            Console.WriteLine(kalkulator.Kolor);
+
+            //juz wczesnij kozrystalismy z wlasciwosci 
+            List<string> list = new List<string>();
+            int liczbaElementow = list.Count;
+            string s = "ala ma kota";
+            int dlugosc = s.Length;
+
+
+
+            Czlowiek czlowiek = new Czlowiek();
+            czlowiek.Nazwisko = "Kowalski";
+            czlowiek.Imie = "JAN";
+            Console.WriteLine(czlowiek.Imie);
+            Console.WriteLine(czlowiek.ImieNazwisko);
+
+            czlowiek.ImieNazwisko = "Adam Nowak";
+
+            ManagerPogody managerPogody = new ManagerPogody();
+
+            managerPogody.SzukanyZnak = "$";
+
+            string s = managerPogody.AdresPodstawowy;
+            managerPogody.AdresPodstawowy = "xx";
+          
+            managerPogody.PodajTemperature("Warszawa", Jednostka.Kelvin);
+        }
+    }
+}
