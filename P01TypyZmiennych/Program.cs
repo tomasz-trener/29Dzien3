2d02ac006da69ef46ac6f4c6d90d4cfbf4c4647b
kurs <kurs@DESKTOP-284UO99>
2024-11-04 10:12:07 +0100

    Add project files.

---
+++ P01TypyZmiennych/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P01TypyZmiennych
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 6; 
+            char b = 'a';
+            string c = "hej";
+            double d = 5.6;
+            bool e = false;
+
+
+            int a2 = a; // przekazanie wartosci 
+            a = 7;  
+            // pytanie co jest w a2 ?  a2 = 6 (kopia)
+            // 
+
+            WebClient wc = new WebClient();
+            DateTime dt = new DateTime();
+            DateTime dt2 = new DateTime(2024, 11, 04);
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("ala");
+            sb.Append("ma");
+            sb.Append("kota");
+            string s = sb.ToString();
+
+            StringBuilder sb2 = sb; // przekazanie referencji 
+            sb.Append("i psa");
+
+            // pytanie co bedzie w sb2  ? w sb2 = "ala ma kota i m psa" 
+            
+            
+            StringBuilder sb3 = new StringBuilder();
+            sb3 = sb2;
+
+            new StringBuilder();
+
+            //using (StringBuilder sb4 = new StringBuilder())
+            //{
+            //    //..
+            //   // ..
+            //}
+
+            DateTime d3 = dt2; 
+            dt2= dt2.AddDays(1);
+            Console.WriteLine(d3.ToString());
+
+            Console.ReadKey();
+
+            // string jest wyjątkiem tzn pomimo tego, ze jest klasą to przekrazujemy argument przez kopie 
+            string c2 = c;
+            c += "Dzien dobry";
+
+            // język C 
+
+            String c3 = "a";
+
+        }
+    }
+}
---
+++ P02WlasneKlasy/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P02WlasneKlasy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kalkulator k = new Kalkulator(); // Tworzymy nowy obiekt typu Kalkulator 
+                                            // i przypisujemy go do nowo zadeklarowanej 
+                                            // zmiennej o nazwie k, typu Kalkulator 
+
+
+
+            Kalkulator k2;  // Deklarujemy zmienną o nazwie 'k2'
+                            // , która jest typu Kalkulator 
+                            // Nie moge teraz pracować na k2 
+                            // ta zmienna jset gotowa na to aby 
+                            // do niej przypisać nowy (lub stary) 
+                            // obiekt typu Kalkulator 
+
+
+            k2 = new Kalkulator(); // tworzymy nowy obiekt typu kalkulator
+                                   // i przypiujemy go do zmiennej k2 
+
+            new Kalkulator(); // tworzymy nowy obiekt w pamieci 
+                              // ale nie mamy do niego uchwytu 
+
+            Kalkulator k3 = k2;  // istniejący obiekt w pamieci do, którego
+                                 // mamy uchwyt przez k2, przypisujemy 
+                                 // do nowo zadeklarowanej zmiennej o nazwie k3, 
+                                 // ,która jest typu Kalkulator 
+                                 // efekt: 2 zmienne, wksazujące na ten sam obiekt 
+        }
+    }
+}
