d1b742fa6a6ba403dd1040cb00199f8aeaee2b84
kurs <kurs@DESKTOP-284UO99>
2024-11-04 15:08:03 +0100

    q

---
+++ P09Konstruktory/Kalkulator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P09Konstruktory
+{
+    internal class Kalkulator
+    {
+        private string kolor;
+
+        private string _tryb; // konwencja nazewnictwa pół prywatnych 
+
+        private int cena;
+
+        //public Kalkulator()
+        //{
+        //    _tryb = "naukowy";
+        //}
+        public Kalkulator(string tryb,string kolor)
+        {
+            _tryb = tryb;
+            this.kolor = kolor;
+        }
+
+        public string Tryb
+        {
+            get { return _tryb; }
+            set { _tryb = value; }
+        }
+
+        public string Policz(double a, double b)
+        {
+            if(_tryb == "naukowy")
+            {
+                return string.Format("{0:0000}", a / b);    
+            }
+            if (_tryb == "normalny")
+            {
+                return string.Format("{0:00}", a / b);
+            }
+
+            throw new Exception("Tryb kalkulatora nieznany");
+        }
+    }
+}
---
+++ P09Konstruktory/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P09Konstruktory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kalkulator kalkulator = new Kalkulator("naukowy","czerwony");
+
+            kalkulator.Tryb = "naukowy";
+
+            string wynik = kalkulator.Policz(10,3);
+
+            ManagerPogody mp = new ManagerPogody(Jednostka.Kelvin);
+
+            double tmp1 = mp.PodajTemperature("warszawa");
+            double tmp2 = mp.PodajTemperature("kraków");
+
+            Console.ReadKey();
+
+            // podsumoawnie 
+
+            // Klasy mogą składać z: 
+            //1) pola -> prywatne cechy obiektu 
+            //2) właściwości -> publiczne cechy obiektu 
+            //3) metody -> sluzace do realizacji zadań
+            //4) konstruktory -> kontrolują sposób tworzenia obiketu 
+        }
+    }
+}
